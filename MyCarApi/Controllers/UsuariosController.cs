38077e1da1e2b4a2146384f1cf4266cd10ac230f
mIINguta <gustavo.minguta@gmail.com>
2024-04-29 14:09:55 -0300

    feat: criando método BuildToken

--- MyCarApi/Controllers/UsuariosController.cs
+++ MyCarApi/Controllers/UsuariosController.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MyCarApi.Models;
 
 namespace MyCarApi.Controllers
@@ -39,11 +43,32 @@ namespace MyCarApi.Controllers
                 ModelState.AddModelError(string.Empty, "Login inválido!");
                 return BadRequest(ModelState);
            }
+        }
+           private UserToken BuildToken(User userModel){
+                var claims = new []{
+                    new Claim(JwtRegisteredClaimNames.UniqueName, userModel.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddHours(1);
+
+            JwtSecurityToken token = new JwtSecurityToken (
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: expiration,
+                signingCredentials: creds);
+                return new UserToken(){
+                    Token = new JwtSecurityTokenHandler().WriteToken(token), Expiration = expiration
+                };
            
+            
 
+           }
 
 
         }
     }
-}
--- MyCarApi/obj/Debug/net8.0/MyCarApi.AssemblyInfo.cs
+++ MyCarApi/obj/Debug/net8.0/MyCarApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCarApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67729dbb6183875ad44e32996e6112847b89c9be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a825074f56b7dc9b1fc2d33dbf709978c91d3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCarApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCarApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
